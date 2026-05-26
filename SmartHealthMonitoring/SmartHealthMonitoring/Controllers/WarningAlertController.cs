978d305787f72e50eed8a9ce00337ecaa67fa6aa
{nqvinh0205} <vinhares1@gmail.com>
2026-05-26 12:40:51 +0700

    feat: add warning alert list and fillter

---
+++ SmartHealthMonitoring/SmartHealthMonitoring/Controllers/WarningAlertController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartHealthMonitoring.Services;
+
+namespace SmartHealthMonitoring.Controllers
+{
+    public class WarningAlertController : Controller
+    {
+        private readonly IWarningAlertService _warningAlertService;
+
+        public WarningAlertController(IWarningAlertService warningAlertService)
+        {
+            _warningAlertService = warningAlertService;
+        }
+
+        public async Task<IActionResult> Index(byte? status)
+        {
+            var alerts = await _warningAlertService
+                .GetAlertsAsync(status);
+
+
+            return View(alerts);
+        }
+    }
+}
